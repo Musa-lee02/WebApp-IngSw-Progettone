c5119960629397b0b4f5bd991560540bcded0487
GodNazzy <nazzylr3@gmail.com>
2023-12-30 11:33:02 +0100

    modifiche routing ed esplora

--- SkillMatch/src/app/component/esplora/esplora.component.ts
+++ SkillMatch/src/app/component/esplora/esplora.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ServizioAnnunciService } from '../../servizio-annunci.service';
 
 @Component({
   selector: 'app-esplora',
   templateUrl: './esplora.component.html',
   styleUrl: './esplora.component.css'
 })
-export class EsploraComponent {
+export class EsploraComponent implements OnInit {
 
-    constructor(private route: ActivatedRoute){
+  ambito : string
+  annunci : any
+    constructor(private service: ServizioAnnunciService, private route: ActivatedRoute){
 
     }
+  ngOnInit(): void {
+    
+    if(this.route.snapshot.paramMap.get('ambito')){
+
+      this.ambito=this.route.snapshot.paramMap.get('ambito')!;
+      this.annunci=this.service.getAnnunciByAmbito(this.ambito);
+    }
+  }
 }
--- SkillMatch/src/app/servizio-annunci.service.ts
+++ SkillMatch/src/app/servizio-annunci.service.ts
@@ -1,29 +1,64 @@
 import { Injectable } from '@angular/core';
 
+
+type Annunci ={
+  
+  img: string
+  descrizione: string
+  ambito: string
+
+}
 @Injectable({
   providedIn: 'root'
 })
+
 export class ServizioAnnunciService {
 
-  url='https://material.angular.io/assets/img/examples/shiba2.jpg'
-  annunci=[{
-    img:'https://material.angular.io/assets/img/examples/shiba2.jpg', 
-  descrizione:"   The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan."+
-  "A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally"+
-  "bred for hunting."},
-  {
+  url='https://material.angular.io/assets/img/examples/shiba2.jpg';
+
+ 
+  
+  annunci : Annunci[]=[{
     img:'https://www.purina.co.uk/sites/default/files/2021-02/BREED%20Hero_0084_miniature_pinscher.jpg',
-    descrizione:"cecedcc"
+    descrizione:"cecedcc",
+    ambito:"Edilizia",
+  },
+  {
+    img:'https://material.angular.io/assets/img/examples/shiba2.jpg',
+    descrizione:"   The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan."+
+      "A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally"+
+      "bred for hunting.",
+    ambito:"Tecnologia"
   }
-]
+  ]
+
   constructor() { }
 
   annunciGetSize(){
     return this.annunci.length
   }
-  annunciGet(){
+  getAnnunci(){
 
     return this.annunci;
+  }
+  getAnnunciByAmbito(ambito: string) : Annunci[]{
+
+    let inHome=true
+    let annunciFiltrati:Annunci[]=[]
+    for(const annuncio of this.annunci){
+
+      if(annuncio.ambito===ambito){
+        inHome=false;
+        
+        annunciFiltrati.push(annuncio)
+      }
+    }
+
+    if(inHome){
+      return this.annunci
+    }
+    return annunciFiltrati
+
   }
 
 }
