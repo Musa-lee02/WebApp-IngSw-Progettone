df4cbddd32b4d99a8b0b05c4825f4347a18b787c
Musa-lee02 <samuelesiciliano.22@gmail.com>
2024-01-04 21:45:27 +0100

    COMMIT SOFFERTO

--- SkillMatch/src/app/component/effettua-annuncio/effettua-annuncio.component.ts
+++ SkillMatch/src/app/component/effettua-annuncio/effettua-annuncio.component.ts
@@ -1,17 +1,47 @@
 import { Component } from '@angular/core';
 import { ServizioAnnunciService } from '../../servizio-annunci.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-effettua-annuncio',
   templateUrl: './effettua-annuncio.component.html',
-  styleUrls: ['./effettua-annuncio.component.css','../profilo/profilo.component.css']
+  styleUrls: ['./effettua-annuncio.component.css']//,'../profilo/profilo.component.css']
 })
-export class EffettuaAnnuncioComponent {
 
+export class EffettuaAnnuncioComponent{
+
+  url = 'https://www.felicinabiorci.com/wp/wp-content/uploads/2019/01/diet-food-macro-111130.jpg'
+
+  province: String[] = ['Cosenza', 'Reggio Calabria', 'Vibo Valentia', 'Catanzaro', 'Crotone',
+              'Napoli', 'Salerno', 'Avellino', 'Benevento', 'Caserta', 'Potenza', 'Matera' ]
+
+  ambiti: String[] = ['Cucina', 'Sport', 'Musica', 'Arte', 'Scienza', 'Informatica', 'Letteratura', 'Cinema', 'Teatro', 'Moda', 'Altro']
+
+  ambitoForm:FormGroup
+  arrowLeft=faArrowLeft
+  
   constructor(private service: ServizioAnnunciService){
 
   }
 
+  ngOnInit(): void {
+    this.ambitoForm=new FormGroup({
+      nomeAnnuncio: new FormControl(null,Validators.required),
+      zonaAnnuncio: new FormControl(null,Validators.required),
+      ambitoAnnucnio: new FormControl(null,Validators.required),
+    })
+  }
+  onSubmit(): void{
+
+  }
+  onSelectFile(e: Event): void {
+  }
+  clickArrow() : void{
+
+  }
+  
   isAutenticato(){
     return this.service.isAutenticato()
   }
