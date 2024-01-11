87d8f74916af10e5875b231142094b735100e603
The-Mob-19 <giuseppemobilio@gmai.com>
2024-01-11 16:10:22 +0100

    da finire

---
+++ SkillMatchBackEnd/src/main/java/pattern/skillmatchbackend/controller/servlet/ConfermaServlet.java
@@ -0,0 +1,50 @@
+package pattern.skillmatchbackend.controller.servlet;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import pattern.skillmatchbackend.model.TokenManager;
+
+import java.io.IOException;
+
+
+@WebServlet("/Conferma")
+public class ConfermaServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String token = req.getParameter("token");
+        String tokenGenerato = TokenManager.verificaToken(token);
+
+        switch(tokenGenerato){
+
+            case "errore":
+                System.out.println("ciao");
+                resp.sendRedirect("/Errore");
+                break;
+
+            case  "scaduto":
+                System.out.println("ciao2");
+                resp.sendRedirect("/Scaduto");
+                break;
+
+            default:
+                System.out.println("ciao3");
+                /*Lavoratore utente = DBManager.getInstance().getLavoratoreDao().findByPrimaryKey(tokenGenerato);
+                if(utente == null) {
+                    resp.sendRedirect("/Errore");
+                }
+                else {
+                    utente.setRegistrato(true);
+                    DBManager.getInstance().getLavoratoreDao().saveOrUpdate(utente);
+                }*/
+                break;
+
+        }
+
+    }
+
+}
