ce1df5e932001ade019f68a178fb6676b9495c78
SAMUELE IANTORNO <samueleiantorno02@gmail.com>
2024-01-19 23:36:13 +0100

    registration

--- Web/src/main/java/application/model/Transaction.java
+++
@@ -1,9 +0,0 @@
-package application.model;
-
-import application.model.user.Customer;
-import application.model.user.Worker;
-
-import java.sql.Date;
-
-public record Transaction(long id, Proposal proposal, float amount, Customer customer, Worker worker, Date date, String paymentMethod) {
-}
--- Web/src/main/java/application/model/email/EmailContent.java
+++
@@ -1,5 +0,0 @@
-package application.model.email;
-
-public record EmailContent(String to, String subject, String body) {
-}
-
