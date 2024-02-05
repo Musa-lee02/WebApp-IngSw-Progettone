74b5c0b7ee7dfebd331cd2f076bb3214c5008581
Musa-lee02 <samuelesiciliano.22@gmail.com>
2024-02-05 20:18:05 +0100

    kiki

--- SkillMatchBackEnd/src/main/java/pattern/skillmatchbackend/controller/servlet/ConfermaServlet.java
+++ SkillMatchBackEnd/src/main/java/pattern/skillmatchbackend/controller/servlet/ConfermaServlet.java
@@ -12,52 +12,16 @@ import pattern.skillmatchbackend.model.Utente;
 import pattern.skillmatchbackend.persistenza.DBManager;
 
 import java.io.IOException;
+import java.util.List;
 
-@CrossOrigin("http://localhost:4200")
 @WebServlet("/ConfermaAccount")
 public class ConfermaServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        resp.setHeader("Access-Control-Allow-Origin", "*");
-
-        String token = req.getParameter("token");
-        String tokenGenerato = TokenManager.verificaToken(token);
-        System.out.println("token: " + tokenGenerato);
-
-        switch(tokenGenerato){
-
-            case "errore":
-                System.out.println("ciao");
-                resp.sendRedirect("/Errore");
-                break;
-
-            case  "scaduto":
-                System.out.println("ciao2");
-                resp.sendRedirect("/Scaduto");
-                break;
-
-            default:
-                System.out.println("ciao3");
-                Utente utente = DBManager.getInstance().getUtenteDao().findByPrimaryKey(tokenGenerato);
-                if (utente != null) {
-                    utente.setRegistrato(true);
-                    DBManager.getInstance().getUtenteDao().saveOrUpdate(utente);
-                }
-
-
-
-
-                if(utente == null) {
-                    resp.sendRedirect("/Errore");
-                }
-                else {
-
-                }
-                break;
-
-        }
-
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/views/conferma_account.html");
+        dispatcher.forward(req, resp);
+        //resp.getWriter().println("<h1>Funziona</h1>");
     }
 
 }
